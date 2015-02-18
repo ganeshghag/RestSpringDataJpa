d8c2e87167b2bc1eaeaba422aa1bb767ad1e6add
Ganesh Ghag <ganesh.ghag@gmail.com>
2015-02-18 17:06:01 +0530

    web app secured with basic auth tested with curl

--- insert.bat
+++ insert.bat
@@ -1,1 +1,1 @@
-curl -i -X POST -H "Content-Type:application/json" -d @insert.cmd http://localhost:8090/people
+curl -i -X POST -H "Content-Type:application/json" --user user:password -d @insert.cmd http://localhost:8090/people
