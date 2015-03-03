bd83e960ceaf61584ffe618d93f0810cbbcc88a8
Ganesh Ghag <ganesh.ghag@gmail.com>
2015-03-03 13:50:57 +0530

    latest

--- tests/contacts.bat
+++ tests/contacts.bat
@@ -1,6 +1,8 @@
 rem add ganesh as trackee
 rem curl -i -X POST -H "Content-Type:application/json" -d {\"name\":\"ganesh\",\"mobile\":\"9920823827\",\"email\":\"ganes@email.com\",\"facebookLink\":\"http://some\",\"twitterLink\":\"http://some\",\"address\":\"addr1\",\"city\":\"Thane\",\"zipcode\":\"737673\",\"imageLink\":\"http://some\"} http://localhost:8090/contacts
 
-curl -i -X POST -H "Content-Type:application/json" -d @contacts.json http://localhost:8090/contacts
+rem curl -i -X POST -H "Content-Type:application/json" -d @contacts.json http://localhost:8090/contacts
+
+curl -i -X POST -H "Content-Type:application/json" --user ganesh:password -d @contacts.json http://54.152.217.134:8080/contacts
 
 
